df4512f60cf3cea0bcd7a17ebdb25336ef06254b
Jeff Thomas <jefferyjthomas@gmail.com>
2014-05-06 15:48:41 -0600

    sad path failing test

--- spec/features/project_manager_creates_todo_spec.rb
+++ spec/features/project_manager_creates_todo_spec.rb
@@ -6,11 +6,19 @@ require 'spec_helper'
   # PM sees newly saved TODO
 
 feature 'Project manager creates TODO' do
+
   scenario 'Successfully' do
     visit new_todo_path
     fill_in 'Description', with: 'Meet up with the team'
     click_button 'Save'
     expect(page).to have_content('Your new TODO was saved')
     expect(page).to have_content('Meet up with the team')
+  end
+
+  scenario 'with description missing' do
+    visit new_todo_path
+    fill_in 'Description', with: 'Meet up with the team'
+    click_button 'Save'
+    expect(page).to have_content('Your new TODO was not saved, please try again.')
   end
 end
