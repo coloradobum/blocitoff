88fb593a36f738f623b869ed6b3ded1da20d4c0a
Jeff Thomas <jefferyjthomas@gmail.com>
2014-05-06 15:43:30 -0600

    Happy path tested.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,23 @@
 class TodosController < ApplicationController
 
   def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to @todo, notice: 'Your new TODO was saved'
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    @todo2 = @todo.description
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description)
   end
 end
