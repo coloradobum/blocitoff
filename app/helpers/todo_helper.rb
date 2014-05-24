5e10bd42472783f3210698b1603f527a6454681c
Jeff Thomas <jefferyjthomas@gmail.com>
2014-05-24 15:00:17 -0600

    Removed files.

--- app/helpers/todo_helper.rb
+++
@@ -1,6 +0,0 @@
-module TodoHelper
-
-  def todo_formatter delta
-    #distance_of_time_in_words( delta, Time.now.utc.to_i, include_seconds: false)
-  end
-end
--- spec/support/create_todo_helpers.rb
+++
@@ -1,8 +0,0 @@
-module Features
-
-  def user_creates_todo(text)
-    visit new_todo_path
-    fill_in 'Description', with: text
-    click_button 'Save'
-  end
-end
