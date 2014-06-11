83a15dd948afe6fc5a210f4d85b01ee46c966310
Jeff Thomas <subskriptions@gmail.com>
2014-06-11 13:30:50 -0600

    'user sees days before deleted' feature green.

--- spec/features/user_sees_days_before_deleted_spec.rb
+++ spec/features/user_sees_days_before_deleted_spec.rb
@@ -6,12 +6,10 @@ feature 'User sees number of days before to-do is deleted',  %q{
   I should see when my task will be deleted.
 } do
   
-  background do
-    user_creates_todo 'Get milk and eggs'
-  end
   
   scenario '7 days before deletion' do
-    visit '/todos'
-    within('days-left') { expect(page).to have_content('7') }
+    create_and_sign_in_user 'user@test.com', 'passwordcrazy'
+    user_creates_todo 'Get milk' 
+    within('td.days-left') { expect(page).to have_content('7 days') }
   end
  end
